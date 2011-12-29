0eb05d242b2923b5d4cc6dcd06fba7511486d5b1
Peter Horn <hornp@provideal.net>
2011-12-29 12:13:17 +0100

    starts to actually do something.

---
+++ example/hello.rb
@@ -0,0 +1,18 @@
+# encoding: utf-8
+
+require '../lib/dinbrief'
+
+Dinbrief.letter('hello.pdf') do |db| 
+  db.return_address 'metaminded UG * Ulmer Straße 176 * 73233 Aalen'
+  db.oursign        "PH"
+  db.yoursign       "MZ"
+  db.yourmessage    "24.12.2011"
+  db.name           "Dr. Peter Horn"
+  db.phone          "05604 - 919937"
+  db.fax            "05604 - 919938"
+  db.email          "ph@metaminded.com"
+  db.subject        "Hooooray!"
+  db.address        "ropa GmbH & Co. KG\n-- Herrn Marco Zapf --\nGoethestraße 5\n77624 Schwäbisch Gmünd\nGERMANY"
+  db.subject        "Frohe Weihnachten von metaminded!"
+end
+
